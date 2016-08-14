e2bc2a11453bf685c0458517f1883c8295bf79ef
roger <rjpd@netzero.com>
2016-08-14 18:13:29 -0400

    mvc5 scaffold a new ctrl, creates all except init.seed

---
+++ WebApp2/Models/Emp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp2.Models
+{
+    public class Emp
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
