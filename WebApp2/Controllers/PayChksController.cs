208859eb75cf0808ec066b6786040b7368e81e6a
roger <rjpd@netzero.com>
2016-08-15 15:02:19 -0400

    view paychks, and bug/typo fix

--- WebApp2/Controllers/PayChksController.cs
+++ WebApp2/Controllers/PayChksController.cs
@@ -46,7 +46,7 @@ namespace WebApp2.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,EmoID,Payee")] PayChk payChk)
+        public ActionResult Create([Bind(Include = "ID,EmpID,Payee")] PayChk payChk)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace WebApp2.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,EmoID,Payee")] PayChk payChk)
+        public ActionResult Edit([Bind(Include = "ID,EmpID,Payee")] PayChk payChk)
         {
             if (ModelState.IsValid)
             {
